5bf9025105b3132629c5f83f2ff9ac0d046ebf0a
invictus-123 <ayushman.mathur7@gmail.com>
2025-08-11 05:19:47 +0530

    Update the vite config to use the gcd bucket url as base

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const gcsBucketUrl = env.VITE_GCS_BUCKET_URL || './';
+
+  return {
+    plugins: [react()],
+    base: gcsBucketUrl,
+  };
+});
