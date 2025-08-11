ef46672665b39742ac195c865c2d02397d008255
invictus-123 <ayushman.mathur7@gmail.com>
2025-08-11 05:42:31 +0530

    ix vite config to properly read environment variables during build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  const gcsBucketUrl = env.VITE_GCS_BUCKET_URL || './';
+export default defineConfig(() => {
+  const gcsBucketUrl = process.env.VITE_GCS_BUCKET_URL || './';
 
   return {
     plugins: [react()],
