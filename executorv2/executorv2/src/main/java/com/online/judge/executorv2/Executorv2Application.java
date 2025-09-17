431cfc3a5ab0bd2ded1c69bdd4c65dce4e5790fd
invictus-123 <ayushman.mathur7@gmail.com>
2025-09-17 23:18:35 +0530

    Initialize the spring boot application

---
+++ executorv2/executorv2/src/main/java/com/online/judge/executorv2/Executorv2Application.java
@@ -0,0 +1,13 @@
+package com.online.judge.executorv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Executorv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Executorv2Application.class, args);
+	}
+
+}
---
+++ executorv2/executorv2/src/test/java/com/online/judge/executorv2/Executorv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online.judge.executorv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Executorv2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
